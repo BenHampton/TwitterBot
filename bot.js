49be1dc93474c39aca7eaf1ae902335c83a6ceb2
BenHampton <BenHampton11@yahoo.com>
2018-06-22 17:18:26 -0500

    pushing latest updates

--- bot.js
+++ bot.js
@@ -1,16 +1,33 @@
-console.log("TESTING: media")
+console.log("TESTING: retweet");
 var Twit = require("twit"); //like in  JAVA Import
 var config = require('./config');  // where config.js contains the twitter keys/tokens
 //creating a new object T
-var T = new Twit (config);
+var T = new Twit(config);
 
-var fs = require('fs');
+var fs = require('fs'); //'fs' = File System
+
 var imgPATH = "./img/ai.png";
+//set up user stream
+var stream = T.stream('user');
 
+
+/***************RETWEET***************/
+  stream.on('tweet', tweetEvent);
+
+  function tweetEvent(eventMsg) {
+    console.log(eventMsg); //Displays user who tweeted @me - JSON
+    var replyTo = eventMsg.in_reply_to_screen_name;
+    var text = eventMsg.text;
+    var from = eventMsg.user.screen_name;
+    console.log(replyTo);
+    console.log(text);
+    console.log(from);
+  }
+
+/***************MEDIA***************/
 //tweetMedia();
-
 function tweetMedia() {
-  console.log("TESTING: were in")
+  console.log("Meida: were in Media")
   var params = {
      encoding: 'base64'
    };
@@ -20,31 +37,29 @@ function tweetMedia() {
   T.post('media/upload', { media_data: b64 }, uploaded);
 
   function uploaded(err, data, response) {
-    console.log("TESTING: uplaoded");
-    //this is where i will tweet
+    console.log("Meida: Uploaded");//this is where i will tweet
+
     var mediaIdStr = data.media_id_string;
-    var altText = "I AM ALT_TEXT0";
+    var altText = "I AM ALT_TEXT";
     var meta_params = {
       alt_text: {text: altText},
       media_id: mediaIdStr
     }
-    console.log(meta_params);
 
     T.post('media/metadata/create', meta_params, uploadImg);
     function uploadImg(err, data, response){
-      console.log("TESTING: uplaodImg");
+      console.log("Meida: uploadImg");
       var tweet = {
         status: '#CodeToMeida0',
         media_ids: [mediaIdStr]
       }
       T.post('statuses/update', tweet, tweetPOST(tweet));
+      console.log("Meida: tweetedIMG")
       }
     }
 }//end of tweetMedia
 
-//set up user stream
-var stream = T.stream("user");
-
+/***************TWEET @user WHO FOLLOWED***************/
 //anytime someone follows me
 stream.on('follow', followed);
 
@@ -52,42 +67,42 @@ function followed(eventMsg) {
   console.log("FOLLOW EVENT")
   var name = eventMsg.source.name;
   var screenName = eventMsg.source.screen_name;
-  tweetPOST('@' + screenName + ' Jorge is my only friend.. LOL!!');
+  var tweet = {
+    status: '@' + screenName + ' Thanks for following me!'
+  };
+  tweetPOST(tweet);
 }
 
+/***************setInterval***************/
 //setInterval(tweetPOST,1000*10);
 
+/***************POST**************/
 function tweetPOST(tweet) {
-  //var r = Math.floor(Math.random()*100);
   //object tweet with element 'status' that is going to be the HashTag
    // var tweet = {
    //   status: txt
    // };
-
   //Post Method, parameters (update tweet,  the parameters of the tweet,   call back function-'tweeted')
   T.post('statuses/update', tweet , tweeted);
-
   function tweeted(err, data, response){
     if(err){
-      //if there is an error print to console
       console.log("Error: ", err);
     }else {
-      //NO ERRORS print to console
       console.log("Tweeting POST");
     }
   }
 }//end of tweetPOST
 
+
+/***************GET***************/
 function tweetGET(){
   //object 'parms' that will be searched via T with 'q'-Query being what HashTag too search for, 'count'-How many results/searches
   var parms = {
-    q: 'testPost_0',
-    count: 3
+    q: 'arielengle',
+    count: 10
    }
   //Get Method, parameters (searching for tweets,  the parameters of the search,   call back function-'gotData')
   T.get('search/tweets', parms, gotData);
-
-  //call back function
   function gotData(err, data, response) {
   // tweet = data(JSON) .  statuses(in parameter in JSON)
     var  tweets = data.statuses;
